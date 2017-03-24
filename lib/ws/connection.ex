0429bf65ab853b7138c4f3f92ce1f78ffa1883e2
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-03-24 12:23:09 +0200

    updated with text

--- lib/ws/connection.ex
+++ lib/ws/connection.ex
@@ -9,11 +9,16 @@ defmodule Ws.Connection do
   def init(state) do
     socket = connect()
     listenerPid = Ws.Listener.start_link(self(), socket)
+    IO.inspect listenerPid
     {:ok, %{socket: socket, listener: listenerPid}}
   end
 
   def connect do
     Socket.Web.connect! @socketUrl
+  end
+
+  def send_text(pid) do
+    GenServer.cast(pid, :send_something)
   end
 
   def ws_send(socket, msg) do
@@ -23,6 +28,14 @@ defmodule Ws.Connection do
   def handle_call(:close, _from, %{listener: pid} = state) do
     Process.exit(pid, :kill)
     {:reply, {}, %{state | listener: nil}}
+  end
+
+  def handle_cast(:send_something, %{socket: socket} = state) do
+    IO.puts "send something"
+    text = "Something"
+    socket 
+    |> ws_send({:text, text})
+    {:noreply, state}
   end
 
   def handle_info(:pong, %{socket: socket} = state) do
